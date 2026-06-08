43db9046d3462455b79f9a77bb8d0120450a2ffe
Nandini Bisht <nandini.bisht.4682@gmail.com>
2026-06-08 14:58:03 +0530

    character_pyramid_pattern

---
+++ character_pyramid_pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+   int n=5;
+   for(int i=1 ; i<=n ; i++){
+    for(int s=i ;s<n ; s++){
+        printf(" ");
+    }
+    for(int j=0 ; j<i ; j++){
+        printf("%c",'A'+ j);
+    }
+    for (int j= i-2 ; j>=0 ; j--){
+        printf("%c",'A'+ j );
+    }
+    printf("\n");
+   }
+   return 0; 
+}
