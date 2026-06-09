a8900c41fb423f0df446a5ccf3663fe5f2f4d629
Nandini Bisht <nandini.bisht.4682@gmail.com>
2026-06-09 20:14:29 +0530

    display_array

---
+++ display_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array elements: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
